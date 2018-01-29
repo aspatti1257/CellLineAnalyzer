6c8f168206292df3c071bf4e5db2b1e7b8e79c7c
Christine Zhang <christinezhang@college.harvard.edu>
2018-01-28 20:19:50 -0500

    dataformattingservice IT

--- Testing/DataFormattingServiceIT.py
+++ Testing/DataFormattingServiceIT.py
@@ -1,7 +1,29 @@
 import unittest
-
+import numpy as np
+import os
 
 class DataFormattingServiceIT(unittest.TestCase):
     #  TODO - Christine: Tests for DataFormattingService. You should be able load the class directly and build out
     #  methods to test the classes directly.
-    pass
+
+    def setUp(self):
+        self.current_working_dir = os.getcwd()  # Should be this package.
+
+    def tearDown(self):
+        pass
+
+    def runTest(self):
+        pass
+
+    def checkimportdata(self):
+        features = np.genfromtxt('SampleClassifierDataFolder/features.csv', delimiter=',')
+        results = np.genfromtxt('SampleClassifierDataFolder/results.csv', delimiter=',')
+        assert np.array(features[1:]).dtype == "float64"
+        assert np.array(results[1:,1]).dtype == "float64"
+        assert np.isnan(features[1:]).any() == False
+        assert np.isnan(results[1:,1]).any() == False
+        assert len(features) == len(results)
+
+
+d = DataFormattingServiceIT()
+print(d.checkimportdata())
