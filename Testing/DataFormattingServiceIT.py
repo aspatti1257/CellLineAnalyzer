4c96a620e5fd8af0dada7ed48bbcb1de03b4bf29
Christine Zhang <christinezhang@college.harvard.edu>
2018-02-05 20:14:03 -0500

    dummy data

--- Testing/DataFormattingServiceIT.py
+++ Testing/DataFormattingServiceIT.py
@@ -1,9 +1,14 @@
 import unittest
 import numpy as np
 import os
+import logging
 
+# from DataFormattingService import DataFormattingService
 
 class DataFormattingServiceIT(unittest.TestCase):
+
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.INFO)
 
     def setUp(self):
         self.current_working_dir = os.getcwd()  # Should be this package.
@@ -19,4 +24,9 @@ class DataFormattingServiceIT(unittest.TestCase):
         assert not np.isnan(features[1:]).any()
         assert not np.isnan(results[1:, 1]).any()
         assert len(features) == len(results)
+
+    # def testCheckOneHotEncoding(self):
+    #     categorical_pd = pd.read_csv('Testing/SampleClassifierDataFolder/categorical.csv', delimiter=',')
+    #     assert (DataFormattingService.one_hot(categorical_pd)).dtype == "float64"
+
 
