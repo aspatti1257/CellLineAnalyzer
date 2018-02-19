bd80762d2da5115bb3b66e4cbf2b1c2501112ca1
aspatti1257 <aspatti1257@gmail.com>
2018-02-18 20:19:53 -0500

    Finished randomized results generator and use it in ArgumentProcessingServiceIT.

--- Testing/ArgumentProcessingServiceIT.py
+++ Testing/ArgumentProcessingServiceIT.py
@@ -14,18 +14,33 @@ class ArgumentProcessingServiceIT(unittest.TestCase):
         self.current_working_dir = os.getcwd()  # Should be this package.
 
     def tearDown(self):
-        pass
+        if self.current_working_dir != "/" :
+            for file in os.listdir(self.current_working_dir + "/" + RandomizedDataGenerator.GENERATED_DATA_FOLDER):
+                os.remove(self.current_working_dir + "/" + RandomizedDataGenerator.GENERATED_DATA_FOLDER + "/" + file)
 
     def testInputTextFileCorrectlyParsed(self):
         input_folder = self.current_working_dir + "/SampleClassifierDataFolder"
+        self.processAndValidateArguments(input_folder, True)
+
+    def testClassifierWithRandomlyGeneratedInput(self):
+        RandomizedDataGenerator.generateRandomizdFiles(5, 50, 150, True)
+        assert len(os.listdir(self.current_working_dir + "/" + RandomizedDataGenerator.GENERATED_DATA_FOLDER)) == 7
+
+        input_folder = self.current_working_dir + "/" + RandomizedDataGenerator.GENERATED_DATA_FOLDER
+        self.processAndValidateArguments(input_folder, True)
+
+    def testRegressorWithRandomlyGeneratedInput(self):
+        RandomizedDataGenerator.generateRandomizdFiles(5, 50, 150, False)
+        assert len(os.listdir(self.current_working_dir + "/" + RandomizedDataGenerator.GENERATED_DATA_FOLDER)) == 7
+
+        input_folder = self.current_working_dir + "/" + RandomizedDataGenerator.GENERATED_DATA_FOLDER
+        self.processAndValidateArguments(input_folder, False)
+
+    def processAndValidateArguments(self, input_folder, is_classifier):
         argument_processing_service = ArgumentProcessingService(input_folder)
         arguments = argument_processing_service.handleInputFolder()
         assert arguments is not None
         assert len(arguments) == 3
         assert (len(arguments.get(argument_processing_service.RESULTS)) + 1) == \
                len(arguments.get(argument_processing_service.FEATURES).keys())
-        assert arguments.get(argument_processing_service.IS_CLASSIFIER)
-
-    def testWithRandomlyGeneratedInput(self):
-        #TODO: finish this test and add more like it for the other ITs.
-        RandomizedDataGenerator.generateCSVs(5, 50, 150, True)
+        assert arguments.get(argument_processing_service.IS_CLASSIFIER) == is_classifier
