a091ae96bc6ad1e0d4a646e5d4658d74f30edbbc
pgeorg1993 <40545819+pgeorg1993@users.noreply.github.com>
2018-09-13 17:13:59 -0400

    alpha = 0.001 unnecessary

--- Trainers/RandomSubsetElasticNetTrainer.py
+++ Trainers/RandomSubsetElasticNetTrainer.py
@@ -40,7 +40,7 @@ class RandomSubsetElasticNetTrainer(AbstractModelTrainer):
 
     def initializeHyperParameters(self):
         return {
-            "alpha": [0.001, 0.01, 0.1, 1, 10],
+            "alpha": [0.01, 0.1, 1, 10],
             "l_one_ratio": [0, 0.1, 0.5, 0.9, 1]
         }
 
