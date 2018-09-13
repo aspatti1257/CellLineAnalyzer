785894eb874170b7f729365cf30270d72edbea54
pgeorg1993 <40545819+pgeorg1993@users.noreply.github.com>
2018-09-13 17:14:30 -0400

    alpha = 0.001 unnecessary

--- Trainers/ElasticNetTrainer.py
+++ Trainers/ElasticNetTrainer.py
@@ -14,7 +14,7 @@ class ElasticNetTrainer(AbstractModelTrainer):
 
     def initializeHyperParameters(self):
         return {
-            "alpha": [0.001, 0.01, 0.1, 1, 10],
+            "alpha": [0.01, 0.1, 1, 10],
             "l_one_ratio": [0, 0.1, 0.5, 0.9, 1]
         }
 
